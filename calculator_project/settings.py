cbf8a9f7a07526db9ab95f1ccfdc37cc8ed9bb64
Ishhod <ishhod419@gmail.com>
2025-05-03 23:07:33 -0400

    Fixed settings.py to allow hosts

--- calculator_project/settings.py
+++ calculator_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4m_c9_%x9#!3x6xtxjv!0&s!wmm_j9izf6ujgbw*)-ksk+%x88
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
